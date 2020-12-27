f0b00f27ed480a77ebdd91628984053a0a87e67b
Aniket Patel <aniket.r.21102001@gmail.com>
2020-12-27 23:59:05 +0530

    add peerjs

--- public/main.js
+++ public/main.js
@@ -1,20 +1,37 @@
-// let userVideo;
-
-// navigator.mediaDevices
-// 	.getUserMedia({
-// 		video: true,
-// 		audio: true,
-// 	})
-// 	.then((response) => {
-// 		userVideo = response;
-// 	});
-
 // const addStrem = () => {};
 
 const socket = io("/");
+const videoGrid = document.getElementById("videoGrid");
 
-socket.emit("join-room", ROOM_ID, 10);
+const peer = new Peer(undefined, {
+	host: "/",
+	port: "3001",
+});
+
+const selfVideo = document.createElement("video");
+selfVideo.muted = true;
+
+navigator.mediaDevices
+	.getUserMedia({
+		video: true,
+		audio: true,
+	})
+	.then((response) => {
+		addVideoStream(selfVideo, response);
+	});
+
+peer.on("open", (id) => {
+	socket.emit("join-room", ROOM_ID, id);
+});
 
 socket.on("user-connected", (userID) => {
 	console.log("User connected -->" + userID);
 });
+
+function addVideoStream(video, stream) {
+	video.srcObject = stream;
+	video.addEventListener("loadedmetadata", () => {
+		video.play();
+	});
+	videoGrid.append(video);
+}
